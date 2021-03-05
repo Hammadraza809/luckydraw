0a2934d43e5189574200865234e0d678a7091267
unknown <mahadkhan809@gmail.com>
2021-03-05 15:22:56 +0500

    register form

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -18,6 +18,30 @@ class Register extends Component {
             </Form.Group>
           </Grid.Col>
         </Grid.Row>
+        <Grid.Row>
+          <Grid.Col md={6} xs={12} sm={12}>
+            <Form.Group label="Input Icon">
+              <Form.Input icon="user" placeholder="Username" />
+            </Form.Group>
+          </Grid.Col>
+          <Grid.Col md={6} xs={12} sm={12}>
+            <Form.Group label="Input Icon">
+              <Form.Input icon="user" placeholder="Username" />
+            </Form.Group>
+          </Grid.Col>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Col md={6} xs={12} sm={12}>
+            <Form.Group label="Input Icon">
+              <Form.Input icon="user" placeholder="Username" />
+            </Form.Group>
+          </Grid.Col>
+          <Grid.Col md={6} xs={12} sm={12}>
+            <Form.Group label="Input Icon">
+              <Form.Input icon="user" placeholder="Username" />
+            </Form.Group>
+          </Grid.Col>
+        </Grid.Row>
       </div>
     );
   }
